524a5300c05bb2c47a66f5d174a8bad40d5a461d
trean <trean.mi@gmail.com>
2022-08-03 19:32:12 +0200

    new page for benchmarks with example plots

---
+++ qdrant-landing/content/benchmarks/benchmark-1.md
@@ -0,0 +1,8 @@
+---
+draft: false
+id: 1
+title: Benchmark 1 Title
+description: The golden tuna fiery endures the wave. Aw! Pieces o" strength are forever swashbuckling.
+data: /benchmarks/data-1.js
+weight: 1
+---
---
+++ qdrant-landing/static/benchmarks/data-1.js
@@ -0,0 +1,16 @@
+export default {
+  labels: [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+  ],
+  datasets: [{
+    label: "My First dataset",
+    backgroundColor: "rgb(255, 99, 132)",
+    borderColor: "rgb(255, 99, 132)",
+    data: [0, 10, 5, 2, 20, 30, 45],
+  }]
+}
