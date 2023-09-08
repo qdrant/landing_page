cea36cc1734f5e57f7c10276031d46bf9296abc8
trean <trean.mi@gmail.com>
2023-09-08 20:18:54 +0200

    fixes of the top menu: top level link, top lvl hover

--- qdrant-landing/themes/qdrant/static/js/script.js
+++ qdrant-landing/themes/qdrant/static/js/script.js
@@ -18,12 +18,6 @@
     $('.main-header li.dropdown .dropdown-btn').on('click', function () {
       $(this).prev('ul').slideToggle(500);
     });
-
-    //Disable dropdown parent link
-    $('.main-header .navigation li.dropdown > a,.hidden-bar .side-menu li.dropdown > a').on('click', function (e) {
-      e.preventDefault();
-    });
-
   }
 
 
