5c209b9b0d748f14f7095cf61aa7c5de06482872
trean <trean.mi@gmail.com>
2023-12-19 16:17:37 +0000

    featured posts carousel

--- qdrant-landing/themes/qdrant/vendor/js/vendor.js
+++ qdrant-landing/themes/qdrant/vendor/js/vendor.js
@@ -4,13 +4,32 @@ import Splide from '@splidejs/splide';
 
 // onload
 document.addEventListener( 'DOMContentLoaded', function() {
+
+  // check if location is a root
+  if (window.location.pathname === '/') {
+
   if (document.querySelector('.splide')) {
     const splide = new Splide('.splide');
 
-    splide.on( 'ready', function () {
+    splide.on('ready', function() {
       const e = new CustomEvent('splideIsReady', {detail: splide});
       document.dispatchEvent(e);
     });
+
+    splide.mount();
+  }
+  }
+
+  // check if location is a blog
+  if (window.location.pathname === '/blog/') {
+    var splide = new Splide( '.splide', {
+      type     : 'loop',
+      gap      : '1rem',
+      arrows   : false,
+      focus    : 0,
+      autoWidth: true,
+      pagination: true,
+    } );
 
     splide.mount();
   }
