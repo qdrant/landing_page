d7f210ccfd7a134a942c81741f9b62a64854ba2b
generall <andrey@vasnetsov.com>
2023-12-22 11:37:49 +0000

    universal image processign script

--- automation/process-blog-img.sh
+++ automation/process-blog-img.sh
@@ -1,7 +1,30 @@
 #!/bin/bash
 
 set -e
-echo "Processing image: $1"
-echo "Alias: $2"
 
-bash -x automation/process-img.sh "$1" "./qdrant-landing/static/blog_data/" "$2"
+if [ -z "$1" ]; then
+  echo "Please provide a path to the image"
+  exit 1
+fi
+
+if [ -z "$2" ]; then
+  echo "Please provide an blog slug"
+  exit 1
+fi
+
+if [ ! -f "$1" ]; then
+  echo "File $1 does not exist"
+  exit 1
+fi
+
+# Get the directory of the script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+
+
+PATH_TO_IMAGE=$1 \
+STATIC_DIRECTORY_NAME="./qdrant-landing/static/blog_data/${2}" \
+SOCIAL_PREVIEW_RESOLUTION="1200x630" \
+TITLE_RESOLUTION="916x515" \
+PREVIEW_RESOLUTION="350x201" \
+bash -x ${DIR}/process-img.sh
--- automation/process-img.sh
+++ automation/process-img.sh
@@ -4,69 +4,41 @@
 
 set -e
 
-Help() {
-  echo "Usage: bash $0 <path to image> <directory name> <blog post name alias> <options>"
-  echo "Example: $0 ./qdrant-landing/static/blog_data/2021-04-01-announcing-qdrant-1-0/qdrant-1-0-social-preview.png ./qdrant-landing/static/blog_data/2021-04-01-announcing-qdrant-1-0/ announcing-qdrant-1-0 1200x630 898x300 530x145"
-  echo "Options:"
-  echo "h     Print this Help."
-  echo "s     Social preview crop in format <width>x<height>. Default: 1200x630"
-  echo "t     Title crop in format <width>x<height>. Default: 898x300"
-  echo "p     Preview crop in format <width>x<height>. Default: 530x145"
-}
 
-while getopts ":hstp" option; do
-   case $option in
-      h) # display Help
-         Help
-         exit;;
-      s) # social preview crop
-          SOCIAL_PREVIEW_CROP_OPT=$OPTARG;;
-      t) # title crop
-          TITLE_CROP_OPT=$OPTARG;;
-      p) # preview crop
-          PREVIEW_CROP_OPT=$OPTARG;;
-      \?) # incorrect option
-          echo "Error: Invalid option"
-          exit;;
-   esac
-done
+# Example usage
+#
+#
+# PATH_TO_IMAGE="./qdrant-landing/static/blog_data/qdrant-v-1-7/preview.png"
+# STATIC_DIRECTORY_NAME="./qdrant-landing/static/blog_data/qdrant-v-1-7"
 
-if [ -z "$1" ]; then
+
+PATH_TO_IMAGE=${PATH_TO_IMAGE:-""}
+STATIC_DIRECTORY_NAME=${STATIC_DIRECTORY_NAME:-""}
+
+
+SOCIAL_PREVIEW_RESOLUTION=${SOCIAL_PREVIEW_RESOLUTION:-"1200x630"}
+TITLE_RESOLUTION=${TITLE_RESOLUTION:-"898x300"}
+PREVIEW_RESOLUTION=${PREVIEW_RESOLUTION:-"530x145"}
+
+
+if [ -z "$PATH_TO_IMAGE" ]; then
   echo "Please provide a path to the image"
   exit 1
 fi
 
-if [ -z "$3" ]; then
-  echo "Please provide an blog post name alias"
-  exit 1
-fi
-
-if [ -z "$2" ]; then
+if [ -z "$STATIC_DIRECTORY_NAME" ]; then
   echo "Please provide an directory name to store images"
   exit 1
 fi
 
-if [ ! -f "$1" ]; then
-  echo "File $1 does not exist"
-  exit 1
-fi
-
-IMG_DESTINATION="${2}/${3}/preview"
+IMG_DESTINATION="${STATIC_DIRECTORY_NAME}/preview"
 mkdir -p $IMG_DESTINATION
 
-SOCIAL_PREVIEW_CROP=$SOCIAL_PREVIEW_CROP_OPT || "1200x630"
-TITLE_CROP=$TITLE_CROP_OPT || "898x300"
-PREVIEW_CROP=$PREVIEW_CROP_OPT || "530x145"
+convert "$1" -resize "${SOCIAL_PREVIEW_RESOLUTION}^" -gravity center -extent $SOCIAL_PREVIEW_RESOLUTION "${IMG_DESTINATION}/social_preview.jpg";
 
-echo "Social preview crop: $SOCIAL_PREVIEW_CROP"
-echo "Title crop: $TITLE_CROP"
-echo "Preview crop: $PREVIEW_CROP"
-
-
-convert "$1" -resize "${SOCIAL_PREVIEW_CROP}^" -gravity center -extent $SOCIAL_PREVIEW_CROP "${IMG_DESTINATION}/social_preview.jpg";
-
-convert "$1" -resize 898x300^ -gravity center -extent 898x300 "${IMG_DESTINATION}/title.jpg";
-convert "${IMG_DESTINATION}/title.jpg" -resize 530x145^ -gravity center -extent 530x145 "${IMG_DESTINATION}/preview.jpg";
+convert "$1" -resize "${TITLE_RESOLUTION}^" -gravity center -extent "${TITLE_RESOLUTION}" "${IMG_DESTINATION}/title.jpg";
+convert "${IMG_DESTINATION}/title.jpg" -resize "${PREVIEW_RESOLUTION}^" -gravity center -extent "${PREVIEW_RESOLUTION}" "${IMG_DESTINATION}/preview.jpg";
 
 cwebp -q 95 "${IMG_DESTINATION}/title.jpg" -o "${IMG_DESTINATION}/title.webp";
 cwebp -q 95 "${IMG_DESTINATION}/preview.jpg" -o "${IMG_DESTINATION}/preview.webp";
+
