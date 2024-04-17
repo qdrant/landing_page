9efb385d26ea83f464e57f45ab42ef50cad399e7
trean <trean.mi@gmail.com>
2024-04-17 14:34:03 +0200

    buttons fix

--- qdrant-landing/themes/qdrant-2024/assets/js/vendor/splide.js
+++ qdrant-landing/themes/qdrant-2024/assets/js/vendor/splide.js
@@ -13,12 +13,6 @@ document.addEventListener('DOMContentLoaded', function () {
       perMove: 1,
       pagination: false,
       padding: { left: 20, right: 20 },
-      classes: {
-        arrows: 'splide__arrows customer-stories__controls',
-        arrow: 'splide__arrow customer-stories__button',
-        prev: 'splide__arrow--prev customer-stories__button-prev',
-        next: 'splide__arrow--next customer-stories__button-next',
-      },
       breakpoints: {
         [MOBILE_WIDTH]: {
           destroy: true,
