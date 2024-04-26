594f3d45b6cb09413686c68bcca51a2de90821d3
trean <trean.mi@gmail.com>
2024-04-26 15:30:03 +0200

    format

--- qdrant-landing/themes/qdrant-2024/assets/js/search/search.js
+++ qdrant-landing/themes/qdrant-2024/assets/js/search/search.js
@@ -2,10 +2,10 @@ import 'qdrant-page-search/dist/js/search.min.js';
 
 (function () {
   if (/documentation/.test(window.location?.pathname)) {
-    window.initQdrantSearch({searchApiUrl: 'https://search.qdrant.tech/api/search', section: "documentation"});
+    window.initQdrantSearch({ searchApiUrl: 'https://search.qdrant.tech/api/search', section: 'documentation' });
   }
 
   if (/blog\/?$/.test(window.location?.pathname)) {
-    window.initQdrantSearch({searchApiUrl: 'https://search.qdrant.tech/api/search', section: "blog"});
+    window.initQdrantSearch({ searchApiUrl: 'https://search.qdrant.tech/api/search', section: 'blog' });
   }
 })();
