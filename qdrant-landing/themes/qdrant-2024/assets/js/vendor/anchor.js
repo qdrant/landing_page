0387ed2f8589230d3ea485c8cb1d054d7755821d
trean <trean.mi@gmail.com>
2024-05-08 18:27:10 +0200

    fixes

--- qdrant-landing/themes/qdrant-2024/assets/js/vendor/anchor.js
+++ qdrant-landing/themes/qdrant-2024/assets/js/vendor/anchor.js
@@ -5,5 +5,5 @@ anchors.options.placement = 'left';
 anchors.options.class = 'text-decoration-none';
 
 document.addEventListener('DOMContentLoaded', function (event) {
-  anchors.add('article :is(h1, h2, h3, h4, h5, h6)');
+  anchors.add('.qdrant-post__body :is(h1, h2, h3, h4, h5, h6)');
 });
