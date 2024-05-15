870d0b96890741df5ec1ba9e67989f2fd683b1b0
trean <trean.mi@gmail.com>
2024-05-15 10:21:02 +0200

    hide cloud video section and run format

--- qdrant-landing/themes/qdrant-2024/assets/js/helpers.js
+++ qdrant-landing/themes/qdrant-2024/assets/js/helpers.js
@@ -2,9 +2,9 @@ export function isElementInViewport(el) {
   var rect = el.getBoundingClientRect();
 
   return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
   );
 }
