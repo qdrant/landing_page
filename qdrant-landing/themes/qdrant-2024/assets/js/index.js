d8baed47202c6f2fd13191e7eedf73e45486f416
trean <trean.mi@gmail.com>
2024-05-15 10:27:14 +0200

    hide cloud video section and run format (#898)

--- qdrant-landing/themes/qdrant-2024/assets/js/index.js
+++ qdrant-landing/themes/qdrant-2024/assets/js/index.js
@@ -1,18 +1,14 @@
 import scrollHandler from './scroll-handler';
 
 // on document ready
-document.addEventListener('DOMContentLoaded', function() {
-
-
-// Header scroll
+document.addEventListener('DOMContentLoaded', function () {
+  // Header scroll
   const body = document.querySelector('body');
   const header = document.querySelector('.site-header');
-  const topBannerHeight = document.querySelector('.top-banner')?.offsetHeight ??
-      0;
-  const mainMenuHeight = document.querySelector('.main-menu')?.offsetHeight ??
-      0;
+  const topBannerHeight = document.querySelector('.top-banner')?.offsetHeight ?? 0;
+  const mainMenuHeight = document.querySelector('.main-menu')?.offsetHeight ?? 0;
   let menuOffset = window.innerWidth >= 1400 ? 24 : 0; // 24px is a PART of padding-top which we want to scroll over
-  window.addEventListener('resize', function() {
+  window.addEventListener('resize', function () {
     menuOffset = window.innerWidth >= 1400 ? 24 : 0;
   });
 
