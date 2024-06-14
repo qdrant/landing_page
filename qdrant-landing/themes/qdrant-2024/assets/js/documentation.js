30c23a7a87f624076f39cc9bb32230a5926b8fe8
trean <trean.mi@gmail.com>
2024-06-14 16:35:38 +0200

    move searchbar outside of mobile docs menu

--- qdrant-landing/themes/qdrant-2024/assets/js/documentation.js
+++ qdrant-landing/themes/qdrant-2024/assets/js/documentation.js
@@ -1,4 +1,5 @@
 import ThemeSwitch from './theme-switch.js';
+import { XL_BREAKPOINT } from './constants';
 
 const themeSwitch = new ThemeSwitch();
 
@@ -6,9 +7,28 @@ document.addEventListener('DOMContentLoaded', () => {
   themeSwitch.initSwitcher();
   const sidebar = document.getElementById('sidebar');
   const sidebarToggle = document.getElementById('sidebar-toggle');
+  const searchbar = document.getElementsByClassName('docs-menu__input')[0];
+  const docsMenu = document.getElementsByClassName('docs-menu')[0];
 
   sidebarToggle.addEventListener('click', () => {
     sidebarToggle.classList.toggle('active');
     sidebar.classList.toggle('active');
   });
+
+  const moveSearchButton = function() {
+    const checkIfFirstChildIsSearch = (element) => {
+      return element.children.length > 0 && element.children[0].classList.contains('docs-menu__input');
+    }
+
+    const isMobile = window.innerWidth <= XL_BREAKPOINT;
+    if (isMobile && !checkIfFirstChildIsSearch(docsMenu)) {
+      docsMenu.before(searchbar);
+    } else if (!isMobile && !checkIfFirstChildIsSearch(sidebar)) {
+      sidebar.before(searchbar);
+    }
+  }
+
+  moveSearchButton();
+
+  window.addEventListener('resize', moveSearchButton);
 });
