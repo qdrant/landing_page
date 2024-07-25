60113d32276f6b3784b588427a1029e5cb93efa3
generall <andrey@vasnetsov.com>
2024-07-25 21:55:12 +0200

    review fixes

--- install-and-build.sh
+++ install-and-build.sh
@@ -2,7 +2,6 @@
 
 DART_SASS_VERSION=${DART_SASS_VERSION:-1.70.0}
 DEPLOY_PRIME_URL=${DEPLOY_PRIME_URL:-"https://qdrant.com"}
-SEGMENT_WRITE_KEY=${SEGMENT_WRITE_KEY:-"noKey"}
 
 CURRENT_DIR=$(pwd)
 
@@ -10,7 +9,4 @@ curl -LJO https://github.com/sass/dart-sass/releases/download/${DART_SASS_VERSIO
     tar -xf dart-sass-${DART_SASS_VERSION}-linux-x64.tar.gz && \
     rm dart-sass-${DART_SASS_VERSION}-linux-x64.tar.gz && \
     export PATH="${CURRENT_DIR}/dart-sass:${PATH}" && \
-    export SEGMENT_WRITE_KEY && \
-    ./update-config.sh && \
-    cd qdrant-landing && \
-    npm install && hugo --gc --minify --config config.toml,config-theme.toml --buildFuture -b ${DEPLOY_PRIME_URL}
+    cd qdrant-landing && npm install && hugo --gc --minify --config config.toml,config-theme.toml --buildFuture -b ${DEPLOY_PRIME_URL}
