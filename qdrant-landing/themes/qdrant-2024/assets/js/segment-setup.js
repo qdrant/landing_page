9d2de9b1703cc9a9c62ee6d5ff7098be3504b97b
Jason Bryant <jason.bryant@qdrant.com>
2024-12-03 18:55:20 +0000

    gather utm params and add to outbound product links

--- qdrant-landing/themes/qdrant-2024/assets/js/segment-setup.js
+++ qdrant-landing/themes/qdrant-2024/assets/js/segment-setup.js
@@ -2,6 +2,10 @@ import * as params from '@params';
 import { setCookie } from './helpers';
 import { setSegmentWriteKey } from './segment-helpers';
 
+document.addEventListener('DOMContentLoaded', () => {
+    addUTMToLinks();
+});
+
 if (params.segmentWriteKey) {
     setSegmentWriteKey(params.segmentWriteKey);
 }
@@ -9,3 +13,32 @@ if (params.segmentWriteKey) {
 if (params.gaMeasurementId) {
     setCookie('ga_measurement_id', params.gaMeasurementId, 365);
 }
+
+function addUTMToLinks() {
+    const urlParams = new URLSearchParams(window.location.search);
+
+    const gclid = urlParams.get('gclid');
+    const utmParams = {
+        source: urlParams.get('utm_source'),
+        medium: urlParams.get('utm_medium'),
+        campaign: urlParams.get('utm_campaign'),
+        campaign: urlParams.get('utm_content')
+    };
+
+    const links = document.querySelectorAll('a[href*="cloud.qdrant.io"]');
+    links.forEach(link => {
+        const href = link.href;
+        const separator = href.indexOf('?') === -1 ? '?' : '&';
+
+        let newHref = `${href}${separator}${gclid ? 'gclid=' + gclid + '&' : ''}`;
+
+        for (const key in utmParams) {
+            if (utmParams[key]) {
+                newHref += `utm_${key}=${utmParams[key]}&`;
+            }
+        }
+
+        newHref = newHref.replace(/[&|?]$/, ''); // remove any trailing ampersands or question marks
+        link.href = newHref;
+    });
+  }
