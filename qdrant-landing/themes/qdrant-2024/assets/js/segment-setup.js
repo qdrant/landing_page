9288b1573802d8773adb2334cbd8a08444e93d7c
Jason Bryant <jason.bryant@qdrant.com>
2024-12-04 07:49:14 +0000

    sessionStorage for gtm params and cleanup

--- qdrant-landing/themes/qdrant-2024/assets/js/segment-setup.js
+++ qdrant-landing/themes/qdrant-2024/assets/js/segment-setup.js
@@ -17,6 +17,7 @@ if (params.gaMeasurementId) {
 function addUTMToLinks() {
     const urlParams = new URLSearchParams(window.location.search);
 
+    // Gather all GTM related params
     const utmIds = {
         gcl: urlParams.get('gclid'),
         gbra: urlParams.get('gbraid'),
@@ -30,26 +31,28 @@ function addUTMToLinks() {
         content: urlParams.get('utm_content')
     };
 
+    // Create new params string for outbound links and store in sessionStorage
+    let newParams = '';
+    for (const key in utmIds) {
+        if (utmIds[key]) {
+            sessionStorage.setItem(`${key}id`, utmIds[key]);
+            newParams += `${key}id=${utmIds[key]}&`;
+        }
+    }
+    for (const key in utmParams) {
+        if (utmParams[key]) {
+            sessionStorage.setItem(`utm_${key}`, utmParams[key]);
+            newParams += `utm_${key}=${utmParams[key]}&`;
+        }
+    }
+    newParams = newParams.replace(/[&|?]$/, ''); // remove trailing & or ?
+
+    // Add url params to outbount links to product site
     const links = document.querySelectorAll('a[href*="cloud.qdrant.io"]');
     links.forEach(link => {
         const href = link.href;
-        const separator = href.indexOf('?') === -1 ? '?' : '&';
-
-        let newHref = `${href}${separator}`;
-
-        for (const key in utmIds) {
-            if (utmIds[key]) {
-                newHref += `${key}id=${utmIds[key]}&`;
-            }
-        }
-
-        for (const key in utmParams) {
-            if (utmParams[key]) {
-                newHref += `utm_${key}=${utmParams[key]}&`;
-            }
-        }
-
-        newHref = newHref.replace(/[&|?]$/, ''); // remove any trailing ampersands or question marks
-        link.href = newHref;
+        const separator = newParams.length === 0 ? '' : href.indexOf('?') === -1 ? '?' : '&';
+        
+        link.href = `${href}${separator}${newParams}`;
     });
   }
