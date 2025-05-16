522cd482cca912815792e3df0ee2f8cd3b88525a
generall <andrey@vasnetsov.com>
2025-05-16 13:56:35 +0200

    update search plugin and use partition meta tag to filter documentation sections

--- qdrant-landing/themes/qdrant-2024/assets/js/search/search.js
+++ qdrant-landing/themes/qdrant-2024/assets/js/search/search.js
@@ -1,15 +1,22 @@
 import 'qdrant-page-search/dist/js/search.min.js';
 
 (function () {
+  // Try to read metadata tag for partition
+  const metadata = document.querySelector('meta[name="partition"]');
+  let partition = null;
+  if (metadata) {
+    partition = metadata.content;
+  }
+
   if (/documentation|docs/.test(window.location?.pathname)) {
-    window.initQdrantSearch({ searchApiUrl: 'https://search.qdrant.tech/api/search', section: 'documentation' });
+    window.initQdrantSearch({ searchApiUrl: 'https://search.qdrant.tech/api/search', section: 'documentation', partition: partition });
   }
 
   if (/articles/.test(window.location?.pathname)) {
-    window.initQdrantSearch({ searchApiUrl: 'https://search.qdrant.tech/api/search', section: 'articles' });
+    window.initQdrantSearch({ searchApiUrl: 'https://search.qdrant.tech/api/search', section: 'articles', partition: partition });
   }
 
   if (/blog/.test(window.location?.pathname)) {
-    window.initQdrantSearch({ searchApiUrl: 'https://search.qdrant.tech/api/search', section: 'blog' });
+    window.initQdrantSearch({ searchApiUrl: 'https://search.qdrant.tech/api/search', section: 'blog', partition: partition });
   }
 })();
