ddcd2fdd1878c915b313f5a46e2c091d89a74b17
Anush008 <mail@anush.sh>
2026-01-28 12:42:57 +0530

    doc: Remove _ from C# snippet
    Signed-off-by: Anush008 <mail@anush.sh>

--- qdrant-landing/content/documentation/headless/snippets/insert-points/update-mode/csharp.cs
+++ qdrant-landing/content/documentation/headless/snippets/insert-points/update-mode/csharp.cs
@@ -7,7 +7,7 @@ public class Snippet
 	{
 		var client = new QdrantClient("localhost", 6334);
 
-		await _client.UpsertAsync(
+		await client.UpsertAsync(
 			new()
 			{
 				CollectionName = "{collection_name}",
