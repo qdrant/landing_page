3d8279f0a003fe1790d0345b11ad65c3f85c4c0e
trean <trean.mi@gmail.com>
2026-02-19 17:31:46 +0100

    Lucene upd (#2142)
    * update of the lucene landing page
    
    * continue updating the lucene landing page
    
    * moved smooth scroll to index.js
    
    * sales calendar page
    
    * moved embedded code to markdown

--- qdrant-landing/themes/qdrant-2024/assets/js/vendor/anchor.js
+++ qdrant-landing/themes/qdrant-2024/assets/js/vendor/anchor.js
@@ -1,6 +1,4 @@
 import AnchorJS from 'anchor-js';
-import { scrollIntoViewWithOffset } from '../helpers';
-import { DOCS_HEADER_OFFSET } from '../constants';
 
 const anchors = new AnchorJS();
 anchors.options.placement = 'left';
@@ -11,22 +9,4 @@ document.addEventListener('DOMContentLoaded', function (event) {
   if (/documentation|articles|course/.test(window.location?.pathname)) {
     anchors.add('.documentation-article > :is(h1, h2, h3, h4, h5, h6)');
   }
-
-  // scroll to anchors:
-  let offset = DOCS_HEADER_OFFSET;
-
-  if (window.location.hash) {
-    scrollIntoViewWithOffset(window.location.hash.replace('#', ''), offset);
-  }
-
-  let allLinks = document.querySelectorAll('a[href^="#"]');
-
-  allLinks.forEach((anchor) => {
-    const target = anchor.getAttribute('href');
-    anchor.addEventListener('click', function (e) {
-      e.preventDefault();
-      history.pushState(null, null, target);
-      scrollIntoViewWithOffset(target.replace('#', ''), offset);
-    });
-  });
 });
