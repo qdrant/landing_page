c3766e2eff223e22410564ba7a16065dcb878922
Luis Cossío <luis.cossio@qdrant.com>
2026-02-20 04:46:42 -0300

    Relevance Feedback docs (#2060)
    * add relevance feedback in Explore page
    
    * Review
    
    * Trigger Build
    
    * Trigger Build
    
    * Create new 'Search Relevance' concept page
    
    * Tweaks
    
    * Update links to moved content
    
    * add rust snippet
    
    * TS anippets
    
    * Clarification about using point IDs
    
    * Add links
    
    * Restructure paragraphs
    
    * docs: Go snippet
    
    Signed-off-by: Anush008 <mail@anush.sh>
    
    * docs: Missed Java snippets with C#
    
    Signed-off-by: Anush008 <mail@anush.sh>
    
    * new: add python snippets
    
    * Make Java and Rust snippets testable
    
    * Remove unnecessary styling
    
    ---------
    
    Signed-off-by: Anush008 <mail@anush.sh>
    Co-authored-by: Evgeniya Sukhodolskaya <suxodolskaya97@gmail.com>
    Co-authored-by: Abdon Pijpelink <abdon.pijpelink@qdrant.com>
    Co-authored-by: Ivan Pleshkov <pleshkov.ivan@gmail.com>
    Co-authored-by: Anush008 <mail@anush.sh>
    Co-authored-by: George Panchuk <george.panchuk@qdrant.tech>

---
+++ qdrant-landing/content/documentation/headless/snippets/query-points-explore/relevance-feedback-naive/typescript.ts
@@ -0,0 +1,23 @@
+import { QdrantClient } from "@qdrant/js-client-rest";
+
+const client = new QdrantClient({ host: "localhost", port: 6333 });
+
+client.query("{collection_name}", {
+    query: {
+        relevance_feedback: {
+            target: [0.1, 0.9, 0.23],
+            feedback: [
+                { example: 111, score: 0.68 },
+                { example: 222, score: 0.72 },
+                { example: 333, score: 0.61 },
+            ],
+            strategy: {
+                naive: {
+                    a: 0.12,
+                    b: 0.43,
+                    c: 0.03,
+                },
+            },
+        },
+    },
+});
