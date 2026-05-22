886a9d4780a18a14d81f081fda9d140bf5f80648
Abdon Pijpelink <abdon.pijpelink@qdrant.com>
2026-05-22 16:33:02 +0200

    Fix C# snippet

--- qdrant-landing/content/documentation/headless/snippets/authentication/bearer/csharp.cs
+++ qdrant-landing/content/documentation/headless/snippets/authentication/bearer/csharp.cs
@@ -8,6 +8,9 @@ public class Snippet
             host: "xyz-example.eu-central.aws.cloud.qdrant.io",
             port: 6334,
             https: true,
+            apiKey: null,
+            grpcTimeout: default,
+            loggerFactory: null,
             headers: new Dictionary<string, string>
             {
                 { "authorization", "Bearer your_token_here" }
--- qdrant-landing/content/documentation/headless/snippets/authentication/bearer/generated/csharp.md
+++ qdrant-landing/content/documentation/headless/snippets/authentication/bearer/generated/csharp.md
@@ -5,6 +5,9 @@ var client = new QdrantClient(
     host: "xyz-example.eu-central.aws.cloud.qdrant.io",
     port: 6334,
     https: true,
+    apiKey: null,
+    grpcTimeout: default,
+    loggerFactory: null,
     headers: new Dictionary<string, string>
     {
         { "authorization", "Bearer your_token_here" }
