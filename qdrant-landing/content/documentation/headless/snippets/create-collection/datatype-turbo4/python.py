deac9dbe32abeb3d50a7e537629374da22b00e08
Abdon Pijpelink <abdon.pijpelink@qdrant.com>
2026-07-28 11:06:22 +0200

    [1.19] Documentation for the Turbo4 datatype (#2517)
    * Add 'turbo4' section to Vectors docs
    
    * Edit existing datatypes sections
    
    * Update outdated 'Vector Datatypes' section on the Collections page
    
    * Edit

---
+++ qdrant-landing/content/documentation/headless/snippets/create-collection/datatype-turbo4/python.py
@@ -0,0 +1,14 @@
+from qdrant_client import QdrantClient, models
+
+# @hide-start
+client = QdrantClient(url="http://localhost:6333")
+# @hide-end
+
+client.create_collection(
+    collection_name="{collection_name}",
+    vectors_config=models.VectorParams(
+        size=1024,
+        distance=models.Distance.COSINE,
+        datatype=models.Datatype.TURBO4,
+    ),
+)
