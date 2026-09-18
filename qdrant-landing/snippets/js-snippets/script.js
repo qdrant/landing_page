fc9eb96615785975915f5833d22cb1eb21c076da
KShivendu <kshivendu1@gmail.com>
2026-09-18 18:50:02 +0530

    chore: drop everything else `git add -A` swept in
    The whole snippets/ tree and raw-raghav2.png were untracked in the working
    tree before this work started; qi-dark.png and shot-after.png were my own
    screenshots. None belong in a charts PR.
    
    All now in .git/info/exclude locally. Staging by explicit path from here.

--- qdrant-landing/snippets/js-snippets/script.js
+++
@@ -1,7 +0,0 @@
-import { QdrantClient } from '@qdrant/js-client-rest';
-
-const client = new QdrantClient({ url: 'http://127.0.0.1:6333' });
-
-client.updateCollection("{collection_name}", {
-    quantization_config: 'Disabled'
-})
